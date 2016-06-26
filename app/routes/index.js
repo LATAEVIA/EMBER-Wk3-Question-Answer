5da6ed6429da0a063b9310196b4f8c4f3c1b6891
LaTaevia Berry <lataevia.berry@gmail.com>
2016-06-26 16:42:09 -0700

    have the route handler add the new question to the Ember Data store
    use params from new-question component to make new question record in
    store and then save it

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,4 +4,11 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('question');
   },
+
+  actions: {
+  save(params) {
+    var newQuestion = this.store.createRecord('quetion', params);
+    newQuestion.save();
+    this.transitionTo('index');
+  },
 });
