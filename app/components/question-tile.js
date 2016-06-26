275c7541b4545b8f05242dd78e315714f8a41f98
LaTaevia Berry <lataevia.berry@gmail.com>
2016-06-26 15:57:31 -0700

    reword components/question-tile.hbs, and add an action handler in question-tile component source file

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,5 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  areNotesShowing: false
+  areNotesShowing: false,
+  actions: {
+    notesShow: function() {
+      this.set('areNotesShowing', true);
+    }
+  }
 });
